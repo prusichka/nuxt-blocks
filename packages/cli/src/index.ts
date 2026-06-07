f43410691c111790b9bdbea548a1a20bd8057315
Denys Prusak <denchuk.prusak@gmail.com>
2026-06-07 14:51:07 +0300

    feat(cli): add block installation system

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,12 +2,15 @@
 
 import { cac } from 'cac'
 import { consola } from 'consola'
+import { addBlock } from './add.js'
 
 const cli = cac('nuxt-blocks')
 
 cli
     .command('add <block>', 'Add a block')
-    .action((block) => {
+    .action(async (block: string) => {
+        await addBlock(block)
+
         consola.success(`Adding "${block}" block`)
     })
 
